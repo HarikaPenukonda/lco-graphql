defa7f2e7b6c55a13cf89db17ade1abac1d7203a
harika penukonda <penukondaharika@gmail.com>
2023-06-04 15:10:46 -0500

    create an express application

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Start your es6 scripts here
+import express from 'express'
+
+const app = express() // Creates an express application
+
+app.get("/",(req,res)=>{
+    res.send("Up and running with grapghql crash course")
+}) // home route
+
+app.listen(8082, () => console.log("Running at 8082"))
