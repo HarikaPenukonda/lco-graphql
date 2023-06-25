b6cc8a8d2d2d54821f40db40a4aecce7ba157809
harika penukonda <penukondaharika@gmail.com>
2023-06-25 15:02:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import express from 'express'
 
-const app = express() // Creates an express application
+import resolvers from './resolvers.js'
+
+import schema from './schema.js'
+
+import {graphqlHTTP} from 'express-graphql'
+
+const app = express() // Creates an express application OcVJSy3hda1kqbbZct_JV
 
 app.get("/",(req,res)=>{
     res.send("Up and running with grapghql crash course")
 }) // home route
+
+//const root = {lco: () => console.log("LearnCodeOnline.in")}
+
+const root = resolvers;
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root, // methods we pass
+    graphiql: true // provided nice interface on the web so we can use it
+}))
 
 app.listen(8082, () => console.log("Running at 8082"))
